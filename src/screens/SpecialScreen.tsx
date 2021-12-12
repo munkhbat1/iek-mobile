81aaffb8fcd9f1192e031d8356d5b1bbd06961d6
BBat-Erdene <BBat-Erdene@users.noreply.github.com>
2021-12-12 15:33:48 +0800

    bottom tabs added

---
+++ src/screens/SpecialScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const SpecialScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Special Screen</Text>
+    </View>
+  );
+};
