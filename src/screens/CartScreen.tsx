924a99e089f20c2116193fcb806919112f5fa58a
BBat-Erdene <BBat-Erdene@users.noreply.github.com>
2021-12-13 22:09:00 +0800

    fix

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -3,8 +3,8 @@ import {View, Text} from 'react-native';
 
 export const CartScreen = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Cart Screen</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white'}}>
+      <Text>Таны карт хоосон байна.</Text>
     </View>
   );
 };
--- src/screens/CategorySearchScreen.tsx
+++ src/screens/CategorySearchScreen.tsx
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 export const CategorySearchScreen = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Cart Screen</Text>
+      <Text>Category Screen</Text>
     </View>
   );
 };
