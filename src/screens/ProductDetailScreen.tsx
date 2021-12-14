3fb795460f61c14aa185648b3a130c575074da8c
BBat-Erdene <BBat-Erdene@users.noreply.github.com>
2021-12-14 15:24:11 +0800

    product detail go back

--- src/screens/ProductDetailScreen.tsx
+++ src/screens/ProductDetailScreen.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text, StyleSheet, Pressable} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {globalStyle} from '../globalStyle';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useNavigation} from '@react-navigation/native';
 
 export const ProductDetailScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Product Detail Screen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Pressable style={styles.backIcon} onPress={() => navigation.goBack()}>
+        <MaterialCommunityIcons
+          name="arrow-left"
+          size={globalStyle.headerIconSize}
+          color={globalStyle.colorPrimary}
+        />
+      </Pressable>
+      <Text style={styles.title}>Holley DTSD545S ухаалаг тоолуур</Text>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    color: globalStyle.colorPrimary,
+    fontSize: 16,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+  },
+  container: {
+    backgroundColor: globalStyle.colorIvory,
+  },
+  backIcon: {
+    paddingLeft: 10,
+    marginBottom: 10,
+  },
+});
