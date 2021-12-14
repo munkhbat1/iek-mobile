eb0befafa315ba74740b8eab18d1397180d1a5f1
BBat-Erdene <BBat-Erdene@users.noreply.github.com>
2021-12-14 13:13:59 +0800

    product card

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -1,11 +1,19 @@
 import React, {FC} from 'react';
-import {Text, StyleSheet, Pressable} from 'react-native';
+import {Text, StyleSheet, Pressable, Image, View} from 'react-native';
 import {globalStyle} from '../../globalStyle';
 
 export const ProductCard: FC<ProductCardProps> = ({item}) => {
   return (
     <Pressable style={styles.cardContainer}>
-      <Text>${item}asd</Text>
+      <Image
+        source={require('../../../assets/images/product-image.png')}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <View style={styles.textContainer}>
+        <Text style={styles.name}>Holley DDSSY 283SR Ухаалаг тоолуур</Text>
+        <Text style={styles.price}>{(630000).toLocaleString()}₮</Text>
+      </View>
     </Pressable>
   );
 };
@@ -27,5 +35,24 @@ const styles = StyleSheet.create({
     shadowOffset: {width: 5, height: 5},
     shadowRadius: 5,
     shadowOpacity: 30,
+  },
+  image: {
+    height: '60%',
+    width: 130,
+    alignSelf: 'center',
+    marginTop: 10,
+  },
+  price: {
+    color: globalStyle.colorPrimary,
+    fontWeight: 'bold',
+  },
+  textContainer: {
+    paddingHorizontal: 10,
+    alignContent: 'space-around',
+    height: '40%',
+  },
+  name: {
+    color: 'black',
+    marginVertical: 10,
   },
 });
