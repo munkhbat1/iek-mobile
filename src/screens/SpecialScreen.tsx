09d7af2ca506148f4dda17f91f8969f0cc684e81
BBat-Erdene <BBat-Erdene@users.noreply.github.com>
2021-12-16 10:35:07 +0800

    dotenv config added

--- src/screens/SpecialScreen.tsx
+++ src/screens/SpecialScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Config from 'react-native-config';
 
 export const SpecialScreen = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Special Screen</Text>
+      <Text>{Config.API_URI}</Text>
     </View>
   );
 };
