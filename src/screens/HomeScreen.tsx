2c86ed9d47ca7a4e8128bf7f29de3b72555d5508
BBat-Erdene <BBat-Erdene@users.noreply.github.com>
2021-12-19 11:48:12 +0800

    horizontal icon category

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {HorizontalIconCategory} from '../components/HorizontalIconCategory';
 import {ProductList} from '../components/Products/ProductList';
 import {globalStyle} from '../globalStyle';
 
 export const HomeScreen = () => {
   return (
     <View style={styles.container}>
+      <HorizontalIconCategory />
       <ProductList />
     </View>
   );
