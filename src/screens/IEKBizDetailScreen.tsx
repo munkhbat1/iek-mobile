924f0d7254290cf1e5f29d420001f4fda0840fa9
BBat-Erdene <BBat-Erdene@users.noreply.github.com>
2021-12-23 21:40:36 +0800

    markdown renderer

--- src/screens/IEKBizDetailScreen.tsx
+++ src/screens/IEKBizDetailScreen.tsx
@@ -4,9 +4,62 @@ import {Text, StyleSheet, Pressable, View, Image} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {globalStyle} from '../globalStyle';
+import Markdown from 'react-native-markdown-renderer';
 
 export const IEKBizDetailScreen = () => {
   const navigation = useNavigation();
+
+  const content = `# h1 Heading 8-)
+  ## h2 Heading
+  ### h3 Heading
+  #### h4 Heading
+  ##### h5 Heading
+  ###### h6 Heading
+  
+  
+  ## Horizontal Rules
+  
+  ___
+  
+  ---
+  
+  ***
+  
+  
+  ## Typographic replacements
+  
+  Enable typographer option to see result.
+  
+  (c) (C) (r) (R) (tm) (TM) (p) (P) +-
+  
+  test.. test... test..... test?..... test!....
+  
+  !!!!!! ???? ,,  -- ---
+  
+  "Smartypants, double quotes" and 'single quotes'
+  
+  
+  ## Emphasis
+  
+  **This is bold text**
+  
+  __This is bold text__
+  
+  *This is italic text*
+  
+  _This is italic text_
+  
+  ~~Strikethrough~~
+  
+  
+  ## Blockquotes
+  
+  
+  > Blockquotes can also be nested...
+  >> ...by using additional greater-than signs right next to each other...
+  > > > ...or with spaces between arrows.
+  `;
+
   return (
     <View style={styles.container}>
       <Pressable style={styles.backButton} onPress={() => navigation.goBack()}>
@@ -23,6 +76,10 @@ export const IEKBizDetailScreen = () => {
           source={require('../../assets/images/statebank.png')}
           style={styles.image}
         />
+        <View style={styles.contentContainer}>
+          <Markdown>{content}</Markdown>
+        </View>
+        <View style={styles.whiteSpace} />
       </ScrollView>
     </View>
   );
@@ -54,5 +111,12 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     height: 200,
     width: '65%',
+  },
+  contentContainer: {
+    marginHorizontal: 10,
+    marginVertical: 30,
+  },
+  whiteSpace: {
+    height: 100,
   },
 });
