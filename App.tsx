cd1d09b82c2933bded750fe356a7140a18924569
BBat-Erdene <BBat-Erdene@users.noreply.github.com>
2022-02-13 11:15:23 +0800

    deep linking

--- App.tsx
+++ App.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {LinkingOptions, NavigationContainer} from '@react-navigation/native';
 import {LeftDrawerNavigation} from './src/navigations/LeftDrawerNavigation';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store';
 
 const App = () => {
+  const linking: LinkingOptions<RootStackParamList> = {
+    prefixes: ['emontaj://'],
+    config: {
+      screens: {
+        Main: {
+          screens: {
+            CartStack: {
+              screens: {
+                Cart: 'cart',
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+
   return (
     <Provider store={store}>
-      <NavigationContainer>
+      <NavigationContainer linking={linking}>
         <LeftDrawerNavigation />
       </NavigationContainer>
     </Provider>
   );
+};
+
+type RootStackParamList = {
+  Main: undefined;
 };
 
 export default App;
